43692ab1cb81165bc0d76ed6bca2b10335d9ed60
Jayanth Kumar <jknani111@gmail.com>
2024-07-28 22:57:30 +0530

    feat: pricing page

---
+++ app/pricing/page.tsx
@@ -0,0 +1,12 @@
+import Pricing from "@/components/pricing"
+
+export const metadata = {
+  title: 'Use Cases - Structured',
+  description: 'Page description',
+}
+
+export default function Page() {
+  return (
+    <Pricing />
+  )
+}
