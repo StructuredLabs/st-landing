6de6100dcffbcfa93f1588512b8e33807a5d06a7
Jayanth Kumar <jknani111@gmail.com>
2024-07-29 00:22:30 +0530

    feat: not found page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
-
-
 import { Inter, Architects_Daughter } from 'next/font/google'
 
+import  '@/app/css/global.css'
 import Header from '@/components/ui/header'
 
 const inter = Inter({
@@ -28,8 +27,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.variable} ${architects_daughter.variable} font-inter antialiased bg-gray-900 text-gray-200 tracking-tight`}>
+    <html lang="en" className='h-full'>
+      <body className={`${inter.variable} ${architects_daughter.variable} font-inter antialiased bg-gray-900 text-gray-200 tracking-tight h-full bg-white`}>
         <div className="flex flex-col min-h-screen overflow-hidden">
           <Header />
           {children}
