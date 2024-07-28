c4e9fea27dfc1f0891f099dae2bd68e8462de0d9
Jayanth Kumar <jknani111@gmail.com>
2024-07-29 00:51:29 +0530

    feat: navbar resources flyout + links update

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -2,14 +2,32 @@
 
 import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-
+import { Bars3Icon, ChevronDownIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import {
+  ChartPieIcon,
+  CursorArrowRaysIcon
+} from '@heroicons/react/24/outline'
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+} from '@headlessui/react'
 const navigation = [
-  { name: 'Home', href: '#' },
-  { name: 'Use-Cases', href: 'https://www.structuredlabs.com/usecases' },
-  { name: 'Pricing', href: 'https://www.structuredlabs.com/pricing' },
-  { name: 'Docs', href: 'https://www.docs.structuredlabs.com/intro' },
-  { name: 'Resource', href: '#' },
+  { name: 'Home', href: '/' },
+  { name: 'Use-Cases', href: '/usecases' },
+  { name: 'Pricing', href: '/pricing' },
+  { name: 'Docs', href: 'https://docs.structuredlabs.com' },
+]
+import { TbLogs } from "react-icons/tb";
+import { MdOutlinePodcasts } from "react-icons/md";
+
+const products = [
+  { name: 'Blog', description: 'Ad anim eiusmod veniam amet aliquip nostrud veniam dolore esse fugiat duis.', href: '#', icon: TbLogs },
+  { name: 'Podcasts', description: 'Voluptate dolor ullamco duis cillum ea nulla nulla Lorem eiusmod elit sunt pariatur ex do.', href: '#', icon: MdOutlinePodcasts },
 ]
 
 export default function Example() {
@@ -24,13 +42,45 @@ export default function Example() {
             <img alt="" src="https://cdn.prod.website-files.com/65d4d703fd133afa1bee8167/65d4da523a7af018c0b99c71_TransparentLogo.png" className="h-8 w-auto" />
           </a>
         </div>
-        <div className="hidden lg:flex lg:gap-x-12">
+        <PopoverGroup className="hidden lg:flex lg:gap-x-12 focus:outline-none">
           {navigation.map((item) => (
             <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
               {item.name}
             </a>
           ))}
-        </div>
+          <Popover className="relative focus:outline-none">
+            <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900 focus:outline-none">
+              Resources
+              <ChevronDownIcon aria-hidden="true" className="h-3 w-3 flex-none text-gray-400" />
+            </PopoverButton>
+
+            <PopoverPanel
+              transition
+              className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
+            >
+              <div className="p-4">
+                {products.map((item) => (
+                  <div
+                    key={item.name}
+                    className="group relative flex gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                  >
+                    <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                      <item.icon aria-hidden="true" className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" />
+                    </div>
+                    <div className="flex-auto">
+                      <a href={item.href} className="block font-semibold text-gray-900">
+                        {item.name}
+                        <span className="absolute inset-0" />
+                      </a>
+                      <p className="mt-1 text-gray-600">{item.description}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+            </PopoverPanel>
+          </Popover>
+        </PopoverGroup>
         <div className="flex flex-1 items-center justify-end gap-x-6">
           <a href="https://app.structuredlabs.com/" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900">
             Log in
@@ -67,10 +117,11 @@ export default function Example() {
               />
             </a>
             <a
-              href="#"
+              href="https://cal.com/structured/30min"
+              target='_blank'
               className="ml-auto rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Sign up
+              Demo
             </a>
             <button
               type="button"
@@ -94,7 +145,25 @@ export default function Example() {
                   </a>
                 ))}
               </div>
-              <div className="py-6">
+              <Disclosure as="div" className="-mx-3 border-none">
+                <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                  Resources
+                  <ChevronDownIcon aria-hidden="true" className="h-5 w-5 flex-none group-data-[open]:rotate-180" />
+                </DisclosureButton>
+                <DisclosurePanel className="mt-2 space-y-2">
+                  {[...products].map((item) => (
+                    <DisclosureButton
+                      key={item.name}
+                      as="a"
+                      href={item.href}
+                      className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.name}
+                    </DisclosureButton>
+                  ))}
+                </DisclosurePanel>
+              </Disclosure>
+              <div className="py-6 border-none">
                 <a
                   href="https://app.structuredlabs.com/"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
