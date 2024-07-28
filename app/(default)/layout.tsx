b58acd0fae72dec6598dcf7b14ed27e694cd4313
Amrutha Gujjar <gujjaramrutha@gmail.com>
2024-07-28 15:02:07 -0700

    Add posthog

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -6,6 +6,9 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 import  '@/app/css/global.css'
 import Footer from '@/components/ui/footer'
+import posthog from 'posthog-js'
+
+posthog.init('phc_P2yhtUehiOqv72K4AYhLhV6BsF6fxabet6oeSVLo3DK', { api_host: 'https://us.i.posthog.com', person_profiles: 'identified_only' })
 
 export default function DefaultLayout({
   children,
