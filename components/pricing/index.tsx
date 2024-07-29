01507664efef68bee8661ebcbed831e9c2cd86e7
Amrutha Gujjar <gujjaramrutha@gmail.com>
2024-07-28 19:18:37 -0700

    Pricing page

--- components/pricing/index.tsx
+++ components/pricing/index.tsx
@@ -13,12 +13,6 @@ import {
 import { Bars3Icon, MinusSmallIcon, PlusSmallIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { CheckIcon } from '@heroicons/react/20/solid'
 
-const navigation = [
-  { name: 'Home', href: '/' },
-  { name: 'Use-Cases', href: '/usecases' },
-  { name: 'Pricing', href: '/pricing' },
-  { name: 'Docs', href: 'https://docs.structuredlabs.com' },
-]
 const pricing = {
   frequencies: [
     { value: 'monthly', label: 'Monthly', priceSuffix: '/month' },
@@ -28,134 +22,102 @@ const pricing = {
     {
       name: 'Free',
       id: 'tier-free',
-      href: '#',
-      price: { monthly: '$15', annually: '$144' },
-      description: 'The essentials to provide your best work for clients.',
-      features: ['5 products', 'Up to 1,000 subscribers', 'Basic analytics'],
+      href: 'https://app.structuredlabs.com/',
+      price: { monthly: '$0', annually: '$0' },
+      description: 'Basic tracking of your business health metrics for small teams.',
+      features: [
+            '3 Data Sources',
+            '5 Users',
+            'Daily Data Refresh',
+            '12 months of historical data',
+            'Team sharing and collaboration features',
+            'Community Support',
+        ],      
       mostPopular: false,
+      buttonText: 'Get started',
     },
     {
       name: 'Growth',
       id: 'tier-growth',
-      href: '#',
-      price: { monthly: '$30', annually: '$288' },
-      description: 'The essentials to provide your best work for clients.',
-      features: ['5 products', 'Up to 1,000 subscribers', 'Basic analytics', '48-hour support response time'],
-      mostPopular: false,
+      href: 'https://app.structuredlabs.com/settings/account',
+      price: { monthly: '$870', annually: '$8352' },
+      description: 'Advanced tracking and analytics for growing businesses.',
+      features: [
+        '10 Data Sources',
+        '50 Users',
+        'Hourly Data Refresh',
+        '24 months of historical data',
+        'Basic Monitoring and Alerting',
+        'Slack Support',
+      ],      
+      mostPopular: true,
+      buttonText: 'Buy plan',
     },
     {
       name: 'Enterprise',
       id: 'tier-enterprise',
-      href: '#',
-      price: { monthly: '$60', annually: '$576' },
+      href: 'https://cal.com/structured/30min',
+      price: { monthly: 'Custom', annually: 'Custom' },
       description: 'A plan that scales with your rapidly growing business.',
       features: [
-        '25 products',
-        'Up to 10,000 subscribers',
-        'Advanced analytics',
-        '24-hour support response time',
-        'Marketing automations',
+        'Unlimited Data Sources',
+        'Unlimited Users',
+        'Real-time Data Refresh',
+        'Unlimited historical data',
+        'API Access with SLA',
+        'Data Export & Import',
+        'Advanced Alerts & Notifications',
+        'Custom Integrations',
+        'Dedicated 24/7 Support',
       ],
-      mostPopular: true,
+      mostPopular: false,
+      buttonText: 'Contact sales',
     },
   ],
 }
+
 const faqs = [
   {
-    question: "What's the best thing about Switzerland?",
-    answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+    question: "Can I switch between plans easily?",
+    answer: "Yes, you can upgrade or downgrade your plan at any time from your account settings. Any changes will take effect immediately, and your billing will be adjusted accordingly."
   },
-  // More questions...
-]
-const footerNavigation = {
-  solutions: [
-    { name: 'Marketing', href: '#' },
-    { name: 'Analytics', href: '#' },
-    { name: 'Commerce', href: '#' },
-    { name: 'Insights', href: '#' },
-  ],
-  support: [
-    { name: 'Pricing', href: '#' },
-    { name: 'Documentation', href: '#' },
-    { name: 'Guides', href: '#' },
-    { name: 'API Status', href: '#' },
-  ],
-  company: [
-    { name: 'About', href: '#' },
-    { name: 'Blog', href: '#' },
-    { name: 'Jobs', href: '#' },
-    { name: 'Press', href: '#' },
-    { name: 'Partners', href: '#' },
-  ],
-  legal: [
-    { name: 'Privacy', href: '#' },
-    { name: 'Terms', href: '#' },
-  ],
-  social: [
-    {
-      name: 'Facebook',
-      href: '#',
-      icon: (props: any) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: 'Instagram',
-      href: '#',
-      icon: (props: any) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: 'X',
-      href: '#',
-      icon: (props: any) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
-        </svg>
-      ),
-    },
-    {
-      name: 'GitHub',
-      href: '#',
-      icon: (props: any) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: 'YouTube',
-      href: '#',
-      icon: (props: any) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-  ],
-}
+  {
+    question: "Is there a free trial for the paid plans?",
+    answer: "While we don't offer a free trial for the paid plans, you can start with the Free plan to get a feel for our platform. When you're ready, upgrading to a paid plan is straightforward and gives you access to more features."
+  },
+  {
+    question: "What happens to my data if I downgrade or cancel my plan?",
+    answer: "If you downgrade or cancel, your data remains safe. However, access to certain features and the amount of historical data may be limited based on your new plan. You can always upgrade again to regain full access."
+  },
+  {
+    question: "How does billing work for the paid plans?",
+    answer: "Billing is straightforward. You can choose between monthly or annual billing cycles. Annual plans offer a discount compared to monthly payments. You can update your billing preferences at any time."
+  },
+  {
+    question: "What kind of support can I expect?",
+    answer: "Support varies by plan. The Free plan offers community support, while the Growth plan includes Slack support for more immediate help. The Enterprise plan provides dedicated 24/7 support for urgent and complex needs."
+  },
+  {
+    question: "Can I customize my plan with add-ons?",
+    answer: "Yes, you can enhance your plan with various add-ons like advanced security, custom integrations, and more frequent data syncs. These can be added as needed to better suit your business requirements."
+  },
+  {
+    question: "Are there any hidden fees?",
+    answer: "No, there are no hidden fees. All costs are transparent and outlined in the pricing details. Any additional services or add-ons will be clearly listed and priced separately."
+  },
+  {
+    question: "How do I contact sales for the Enterprise plan?",
+    answer: "You can contact our sales team by clicking the 'Contact sales' button on the Enterprise plan section. They'll assist you in customizing the plan to meet your specific needs and answer any questions you might have."
+  },
+  {
+    question: "What is the impact of using real-time data refresh in the Enterprise plan?",
+    answer: "Real-time data refresh ensures that you always have the most up-to-date information, which is crucial for making timely and informed business decisions. This feature can significantly improve your ability to respond to market changes and operational needs."
+  },
+  {
+    question: "How does the Slack support in the Growth plan work?",
+    answer: "Slack support allows you to quickly get in touch with our support team directly through Slack. This means faster response times and more efficient resolution of any issues you encounter, keeping your operations running smoothly."
+  }
+];
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
@@ -176,8 +138,7 @@ export default function Pricing() {
             </p>
           </div>
           <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600">
-            Choose an affordable plan that’s packed with the best features for engaging your audience, creating customer
-            loyalty, and driving sales.
+            Get real-time insights, smart analytics, and instant alerts. Make sharper decisions and watch your business grow.
           </p>
           <div className="mt-16 flex justify-center">
             <fieldset aria-label="Payment frequency">
@@ -220,7 +181,9 @@ export default function Pricing() {
                 <p className="mt-6 flex items-baseline gap-x-1">
                     {/* @ts-ignore */}
                   <span className="text-4xl font-bold tracking-tight text-gray-900">{tier.price[frequency.value]}</span>
-                  <span className="text-sm font-semibold leading-6 text-gray-600">{frequency.priceSuffix}</span>
+                  {tier.name === 'Growth' && (
+                    <span className="text-sm font-semibold leading-6 text-gray-600">{frequency.priceSuffix}</span>
+                  )}
                 </p>
                 <a
                   href={tier.href}
@@ -232,7 +195,7 @@ export default function Pricing() {
                     'mt-6 block rounded-md px-3 py-2 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600',
                   )}
                 >
-                  Buy plan
+                  {tier.buttonText}
                 </a>
                 <ul role="list" className="mt-8 space-y-3 text-sm leading-6 text-gray-600">
                   {tier.features.map((feature) => (
@@ -244,106 +207,6 @@ export default function Pricing() {
                 </ul>
               </div>
             ))}
-          </div>
-        </div>
-
-        {/* Logo cloud */}
-        <div className="mx-auto mt-24 max-w-7xl px-6 sm:mt-32 lg:px-8">
-          <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-            <img
-              alt="Transistor"
-              src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
-              width={158}
-              height={48}
-              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            />
-            <img
-              alt="Reform"
-              src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
-              width={158}
-              height={48}
-              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            />
-            <img
-              alt="Tuple"
-              src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
-              width={158}
-              height={48}
-              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            />
-            <img
-              alt="SavvyCal"
-              src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
-              width={158}
-              height={48}
-              className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-            />
-            <img
-              alt="Statamic"
-              src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
-              width={158}
-              height={48}
-              className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-            />
-          </div>
-          <div className="mt-16 flex justify-center">
-            <p className="relative rounded-full bg-gray-50 px-4 py-1.5 text-sm leading-6 text-gray-600 ring-1 ring-inset ring-gray-900/5">
-              <span className="hidden md:inline">
-                Transistor saves up to $40,000 per year, per employee by working with us.
-              </span>
-              <a href="#" className="font-semibold text-blue-600">
-                <span aria-hidden="true" className="absolute inset-0" /> See our case study{' '}
-                <span aria-hidden="true">&rarr;</span>
-              </a>
-            </p>
-          </div>
-        </div>
-
-        {/* Testimonial section */}
-        <div className="mx-auto mt-24 max-w-7xl sm:mt-56 sm:px-6 lg:px-8">
-          <div className="relative overflow-hidden bg-gray-900 px-6 py-20 shadow-xl sm:rounded-3xl sm:px-10 sm:py-24 md:px-12 lg:px-20">
-            <img
-              alt=""
-              src="https://images.unsplash.com/photo-1601381718415-a05fb0a261f3?ixid=MXwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8ODl8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1216&q=80"
-              className="absolute inset-0 h-full w-full object-cover brightness-150 saturate-0"
-            />
-            <div className="absolute inset-0 bg-gray-900/90 mix-blend-multiply" />
-            <div aria-hidden="true" className="absolute -left-80 -top-56 transform-gpu blur-3xl">
-              <div
-                style={{
-                  clipPath:
-                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                }}
-                className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-[#ff4694] to-[#776fff] opacity-[0.45]"
-              />
-            </div>
-            <div
-              aria-hidden="true"
-              className="hidden md:absolute md:bottom-16 md:left-[50rem] md:block md:transform-gpu md:blur-3xl"
-            >
-              <div
-                style={{
-                  clipPath:
-                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                }}
-                className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-[#ff4694] to-[#776fff] opacity-25"
-              />
-            </div>
-            <div className="relative mx-auto max-w-2xl lg:mx-0">
-              <img alt="" src="https://tailwindui.com/img/logos/workcation-logo-white.svg" className="h-12 w-auto" />
-              <figure>
-                <blockquote className="mt-6 text-lg font-semibold text-white sm:text-xl sm:leading-8">
-                  <p>
-                    “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente
-                    alias molestiae. Numquam corrupti in laborum sed rerum et corporis.”
-                  </p>
-                </blockquote>
-                <figcaption className="mt-6 text-base text-white">
-                  <div className="font-semibold">Judith Black</div>
-                  <div className="mt-1">CEO of Workcation</div>
-                </figcaption>
-              </figure>
-            </div>
           </div>
         </div>
 
