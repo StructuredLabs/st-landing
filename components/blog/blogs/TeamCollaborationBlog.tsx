3fe1c9d2f857a85c81df8dbdf6581d67c0b4ea9c
Jayanth Kumar <jknani111@gmail.com>
2024-07-30 22:07:09 +0530

    feat(blog): Browse blogs

---
+++ components/blog/blogs/TeamCollaborationBlog.tsx
@@ -0,0 +1,27 @@
+import { CheckCircleIcon, InformationCircleIcon } from '@heroicons/react/20/solid'
+import BlogContent from "../blogcontent";
+
+export default function TeamCollaborationBlog({
+    prefix = 'Introducing',
+    title = "JavaScript for Beginners",
+    description = "Pariatur velit exercitation pariatur do et sunt. Occaecat irure officia pariatur voluptate ut dolore.",
+}: {
+    prefix?: string
+    title: string
+    description: string
+}) {
+    return (
+        <BlogContent
+            prefix={prefix}
+            title={title}
+            description={description}
+        >
+
+            <div className="mt-10 max-w-2xl">
+                <p>
+                    {/* TODO */}
+                </p>
+            </div>
+        </BlogContent>
+    )
+}
