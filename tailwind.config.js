e71eed3c597e9fee87b7d3052e97d2dcae7249cc
Amrutha Gujjar <gujjaramrutha@gmail.com>
2024-10-21 14:16:21 -0700

    hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,15 @@ module.exports = {
           from: { transform: "translateX(0)" },
           to: { transform: "translateX(calc(-100% - 4rem))" },
         },
+        gradient: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
       },
       animation: {
         "logo-cloud": "logo-cloud 30s linear infinite", // Adjust the duration as necessary.
+        "gradient-move": "gradient 3s ease infinite", // Faster gradient animation.
       },
     },
   },
