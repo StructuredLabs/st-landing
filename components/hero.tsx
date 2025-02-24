6e291b6476738b408f785f82d7842c3d6064ec70
Amrutha Gujjar <gujjaramrutha@gmail.com>
2025-02-23 20:59:27 -0800

    Replace docs link

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,6 +1,6 @@
 import AppScreenshot from '@/components/appscreenshot'
 
-export const heroPageTitle = "Code, connect and deploy interactive data apps faster than ever."; 
+export const heroPageTitle = "Code, connect and deploy interactive data apps faster than ever.";
 
 export const heroPageSubtitle =
   "Ingest, transform, and visualize your data in minutes. No heavy infrastructure. No steep learning curve.";
@@ -14,10 +14,10 @@ export default function Hero() {
 
         {/* Main content */}
         <div className="py-24 sm:py-24 lg:pb-32 relative z-10">
-          
+
           {/* Y Combinator Badge */}
           <YCombinatorBadge />
-          
+
           {/* Hero Content */}
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl text-center mb-8">
@@ -25,7 +25,7 @@ export default function Hero() {
               <p className="mt-6 text-xl leading-8 text-gray-600">
                 {heroPageSubtitle}
               </p>
-              
+
               {/* Call to Action Buttons */}
               <CallToActionButtons />
             </div>
@@ -46,10 +46,10 @@ function YCombinatorBadge() {
       <p className="relative rounded-full bg-gray-50 mb-6 px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-inset ring-gray-900/5 flex items-center">
         <img src="/images/investors/YC.png" alt="YC Logo" className="h-4 w-4 mr-2 ml-2" />
         <span className="hidden md:inline">Backed by Y Combinator&nbsp; &nbsp;</span>
-        <a href="https://www.ycombinator.com/companies/structured" 
+        <a href="https://www.ycombinator.com/companies/structured"
           target="_blank"
           rel="noopener noreferrer"
-        className="font-semibold text-slate-800">
+          className="font-semibold text-slate-800">
           <span aria-hidden="true" className="absolute inset-0" />
           {' '} Read more {' '}
           <span aria-hidden="true">&rarr;</span>
@@ -74,7 +74,7 @@ function CallToActionButtons() {
     <div className="mt-10 flex flex-col items-center justify-center relative z-20">
       <div className="flex items-center justify-center gap-x-6">
         <a
-          href="https://app.structuredlabs.com/"
+          href="https://console.structuredlabs.com/"
           target="_blank"
           rel="noopener noreferrer"
           className="rounded-md bg-slate-900 px-3 py-2 text-md font-semibold text-white shadow-sm hover:bg-slate-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600"
@@ -82,7 +82,7 @@ function CallToActionButtons() {
           Get started
         </a>
         <a
-          href="https://docs.structuredlabs.com"
+          href="https://docs.preswald.com"
           target="_blank"
           className="text-md font-semibold leading-6 text-gray-900"
         >
--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -6,7 +6,7 @@ const navigation = {
     { name: "GitHub", href: "https://github.com/StructuredLabs/preswald" },
     { name: "Pricing", href: "/pricing" },
     { name: "Contact", href: "https://cal.com/structured" },
-    { name: "Documentation", href: "https://docs.structuredlabs.com" },
+    { name: "Documentation", href: "https://docs.preswald.com" },
     { name: "Blog", href: "https://blog.structuredlabs.com" },
     { name: "Status", href: "https://structured.instatus.com/" },
   ],
@@ -23,7 +23,7 @@ const navigation = {
     {
       name: "X",
       href: "https://x.com/StructuredLabs",
-      icon: (props:any) => (
+      icon: (props: any) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
         </svg>
@@ -37,7 +37,7 @@ const navigation = {
     {
       name: "GitHub",
       href: "https://github.com/StructuredLabs/",
-      icon: (props:any) => (
+      icon: (props: any) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
@@ -50,7 +50,7 @@ const navigation = {
     {
       name: "YouTube",
       href: "https://www.youtube.com/@Structured-Labs",
-      icon: (props:any) => (
+      icon: (props: any) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
