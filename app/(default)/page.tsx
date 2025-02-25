30d6e0aa40db5a2d38d743d29d577e1deaf2e87b
Amrutha Gujjar <gujjaramrutha@gmail.com>
2025-02-24 17:46:11 -0800

    New branding

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,26 +1,28 @@
 export const metadata = {
-  title: 'Home | Structured',
-  description: 'Automating data discovery with AI',
+  title: 'Structured Labs',
+  description: 'Applied research lab focused on building tools for developers and data scientists.',
 }
 
-import Hero from '@/components/hero'
-import Features from '@/components/features'
-import Bento from '@/components/bento'
-import Blog from '@/components/blog'
-import CTA from '@/components/cta'
-import Testimonial from '@/components/testimonial'
-import Logos from '@/components/logos'
+// import Hero from '@/components/hero'
+// import Features from '@/components/features'
+// import Bento from '@/components/bento'
+// import Blog from '@/components/blog'
+// import CTA from '@/components/cta'
+// import Testimonial from '@/components/testimonial'
+// import Logos from '@/components/logos'
+import Mission from '@/components/mission'
 
 export default function Home() {
   return (
     <>
-      <Hero />
+      <Mission />
+      {/* <Hero />
       <Logos />
       <Bento />
       <Features />
       <Testimonial />
       <Blog />
-      <CTA />
+      <CTA /> */}
     </>
   )
 }
--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,154 +1,68 @@
-import { FaLinkedin } from "react-icons/fa6";
+import { FaLinkedin, FaXTwitter, FaGithub, FaSlack } from "react-icons/fa6";
 import { AiFillSlackCircle } from "react-icons/ai";
 
 const navigation = {
   company: [
-    { name: "GitHub", href: "https://github.com/StructuredLabs/preswald" },
-    { name: "Pricing", href: "/pricing" },
-    { name: "Contact", href: "https://cal.com/structured" },
-    { name: "Documentation", href: "https://docs.preswald.com" },
-    { name: "Blog", href: "https://blog.structuredlabs.com" },
-    { name: "Status", href: "https://structured.instatus.com/" },
-  ],
-  legal: [
-    { name: "Privacy Policy", href: "/privacy-policy" },
-    { name: "Terms of Service", href: "/terms-of-service" },
+    { name: 'Product', href: 'https://www.preswald.com' },
+    { name: 'Contact', href: 'mailto:founders@structuredlabs.com' },
+    { name: 'Blog', href: 'https://www.blog.preswald.com/' },
+    { name: 'Documentation', href: 'https://docs.preswald.com' },
   ],
   social: [
     {
-      name: "Linkedin",
-      href: "https://www.linkedin.com/company/structuredlabs/",
-      icon: FaLinkedin,
+      name: 'Linkedin',
+      href: 'https://www.linkedin.com/company/structuredlabs/',
+      icon: (props: any) => <FaLinkedin className="h-6 w-6" />,
     },
     {
-      name: "X",
-      href: "https://x.com/StructuredLabs",
-      icon: (props: any) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
-        </svg>
-      ),
+      name: 'Twitter',
+      href: 'https://www.x.com/structuredlabs',
+      icon: (props: any) => <FaXTwitter className="h-6 w-6" />,
     },
     {
-      name: "Slack",
-      href: "https://structured-users.slack.com/join/shared_invite/zt-265ong01f-UHP6BP3FzvOmMQDIKty_JQ#/shared-invite/email",
-      icon: AiFillSlackCircle,
+      name: 'GitHub',
+      href: 'https://github.com/StructuredLabs/preswald',
+      icon: (props: any) => <FaGithub className="h-6 w-6" />,
     },
     {
-      name: "GitHub",
-      href: "https://github.com/StructuredLabs/",
-      icon: (props: any) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: "YouTube",
-      href: "https://www.youtube.com/@Structured-Labs",
-      icon: (props: any) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
+      name: 'Slack',
+      href: 'https://structured-users.slack.com/join/shared_invite/zt-265ong01f-UHP6BP3FzvOmMQDIKty_JQ#/shared-invite/email-',
+      icon: (props: any) => <FaSlack className="h-6 w-6" />,
     },
   ],
 };
 
 export default function Footer() {
   return (
-    <footer className="bg-white">
-      <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
-        <div className="flex flex-wrap justify-between">
-          {/* Left Section */}
-          <div className="w-full lg:w-1/3 space-y-8">
-            <img
-              alt="Structured"
-              src="https://cdn.prod.website-files.com/65d4d703fd133afa1bee8167/65d4da523a7af018c0b99c71_TransparentLogo.png"
-              className="h-7"
-            />
-            <div className="flex items-center space-x-4 mt-8">
-              <span className="text-sm text-gray-600">Backed by:</span>
-              <a
-                href="https://www.ycombinator.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src="/images/investors/YC.png" alt="YC" className="h-10" />
-              </a>
-              <a
-                href="https://www.generalcatalyst.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src="/images/investors/GC.png" alt="GC" className="h-10" />
-              </a>
-            </div>
-            <div className="flex space-x-6 mt-8">
-              {navigation.social.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-gray-400 hover:text-gray-500"
-                >
-                  <item.icon aria-hidden="true" className="h-6 w-6" />
-                </a>
-              ))}
-            </div>
-          </div>
-
-          {/* Right Section */}
-          <div className="w-full mt-16 grid grid-cols-2 gap-8 lg:w-2/3 lg:mt-0 lg:grid-cols-4">
-            <div>
-              <h3 className="text-sm font-semibold text-gray-900">Company</h3>
-              <ul className="mt-4 space-y-2">
-                {navigation.company.map((item) => (
-                  <li key={item.name}>
-                    <a
-                      href={item.href}
-                      className="text-sm text-gray-600 hover:text-gray-900"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {item.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div>
-              <h3 className="text-sm font-semibold text-gray-900">Legal</h3>
-              <ul className="mt-4 space-y-2">
-                {navigation.legal.map((item) => (
-                  <li key={item.name}>
-                    <a
-                      href={item.href}
-                      className="text-sm text-gray-600 hover:text-gray-900"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {item.name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
+    <footer aria-labelledby="footer-heading" className="bg-white">
+      <div className="border-t border-gray-900/10 py-4 px-6 flex flex-col sm:flex-row items-center justify-between gap-4">
+        <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-4">
+          {navigation.company.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-sm leading-6 text-gray-600 hover:text-gray-900"
+            >
+              {item.name}
+            </a>
+          ))}
         </div>
-        <div className="mt-16 border-t border-gray-900/10 pt-8">
-          <p className="text-xs text-gray-500">
-            &copy; 2025 Structured Labs, Inc. All rights reserved.
-          </p>
+
+        <div className="flex items-center space-x-4">
+          {navigation.social.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-gray-500"
+            >
+              <span className="sr-only">{item.name}</span>
+              <item.icon aria-hidden="true" className="h-6 w-6" />
+            </a>
+          ))}
         </div>
       </div>
     </footer>
