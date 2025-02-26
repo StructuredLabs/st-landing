e177a4a49a82d8ed016537d4a57f8838d7105b6d
Amrutha Gujjar <gujjaramrutha@gmail.com>
2025-02-26 02:37:59 -0800

    Update footer.tsx

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -11,22 +11,22 @@ const navigation = {
     {
       name: "Linkedin",
       href: "https://www.linkedin.com/company/structuredlabs/",
-      icon: () => <FaLinkedin className="h-6 w-6" />,
+      icon: () => <FaLinkedin className="h-5 w-5 sm:h-6 sm:w-6" />,
     },
     {
       name: "Twitter",
       href: "https://www.x.com/structuredlabs",
-      icon: () => <FaXTwitter className="h-6 w-6" />,
+      icon: () => <FaXTwitter className="h-5 w-5 sm:h-6 sm:w-6" />,
     },
     {
       name: "GitHub",
       href: "https://github.com/StructuredLabs/preswald",
-      icon: () => <FaGithub className="h-6 w-6" />,
+      icon: () => <FaGithub className="h-5 w-5 sm:h-6 sm:w-6" />,
     },
     {
       name: "Slack",
       href: "https://preswald-community.slack.com/join/shared_invite/zt-265ong01f-UHP6BP3FzvOmMQDIKty_JQ",
-      icon: () => <FaSlack className="h-6 w-6" />,
+      icon: () => <FaSlack className="h-5 w-5 sm:h-6 sm:w-6" />,
     },
   ],
 };
@@ -34,24 +34,24 @@ const navigation = {
 export default function Footer() {
   return (
     <footer aria-labelledby="footer-heading" className="bg-white">
-      <div className="border-t border-gray-900/10 py-4 px-6 flex flex-col sm:flex-row items-center justify-between gap-4">
-        {/* First row (Links) */}
-        <div className="flex flex-wrap justify-center gap-4">
+      <div className="border-t border-gray-900/10 py-4 px-6 flex flex-col items-center gap-3 sm:flex-row sm:justify-between">
+        {/* First row: Company links */}
+        <div className="flex flex-nowrap justify-center gap-3 overflow-x-auto text-xs sm:text-sm">
           {navigation.company.map((item) => (
             <a
               key={item.name}
               href={item.href}
               target="_blank"
               rel="noopener noreferrer"
-              className="text-sm text-gray-600 hover:text-gray-900"
+              className="whitespace-nowrap text-gray-600 hover:text-gray-900"
             >
               {item.name}
             </a>
           ))}
         </div>
 
-        {/* Second row (Socials) */}
-        <div className="flex justify-center gap-4">
+        {/* Second row: Social icons */}
+        <div className="flex justify-center gap-3">
           {navigation.social.map((item) => (
             <a
               key={item.name}
