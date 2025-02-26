714755d9c777834eaf4b9b6b492f4a35e6711a19
Amrutha Gujjar <gujjaramrutha@gmail.com>
2025-02-26 02:33:28 -0800

    Update footer.tsx

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,33 +1,32 @@
 import { FaLinkedin, FaXTwitter, FaGithub, FaSlack } from "react-icons/fa6";
-import { AiFillSlackCircle } from "react-icons/ai";
 
 const navigation = {
   company: [
-    { name: 'Product', href: 'https://www.preswald.com' },
-    { name: 'Contact', href: 'mailto:founders@structuredlabs.com' },
-    { name: 'Blog', href: 'https://www.blog.structuredlabs.com/' },
-    { name: 'Documentation', href: 'https://docs.preswald.com' },
+    { name: "Product", href: "https://www.preswald.com" },
+    { name: "Contact", href: "mailto:founders@structuredlabs.com" },
+    { name: "Blog", href: "https://www.blog.structuredlabs.com/" },
+    { name: "Documentation", href: "https://docs.preswald.com" },
   ],
   social: [
     {
-      name: 'Linkedin',
-      href: 'https://www.linkedin.com/company/structuredlabs/',
-      icon: (props: any) => <FaLinkedin className="h-6 w-6" />,
+      name: "Linkedin",
+      href: "https://www.linkedin.com/company/structuredlabs/",
+      icon: () => <FaLinkedin className="h-6 w-6" />,
     },
     {
-      name: 'Twitter',
-      href: 'https://www.x.com/structuredlabs',
-      icon: (props: any) => <FaXTwitter className="h-6 w-6" />,
+      name: "Twitter",
+      href: "https://www.x.com/structuredlabs",
+      icon: () => <FaXTwitter className="h-6 w-6" />,
     },
     {
-      name: 'GitHub',
-      href: 'https://github.com/StructuredLabs/preswald',
-      icon: (props: any) => <FaGithub className="h-6 w-6" />,
+      name: "GitHub",
+      href: "https://github.com/StructuredLabs/preswald",
+      icon: () => <FaGithub className="h-6 w-6" />,
     },
     {
-      name: 'Slack',
-      href: 'https://preswald-community.slack.com/join/shared_invite/zt-265ong01f-UHP6BP3FzvOmMQDIKty_JQ',
-      icon: (props: any) => <FaSlack className="h-6 w-6" />,
+      name: "Slack",
+      href: "https://preswald-community.slack.com/join/shared_invite/zt-265ong01f-UHP6BP3FzvOmMQDIKty_JQ",
+      icon: () => <FaSlack className="h-6 w-6" />,
     },
   ],
 };
@@ -36,21 +35,23 @@ export default function Footer() {
   return (
     <footer aria-labelledby="footer-heading" className="bg-white">
       <div className="border-t border-gray-900/10 py-4 px-6 flex flex-col sm:flex-row items-center justify-between gap-4">
-        <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-4">
+        {/* First row (Links) */}
+        <div className="flex flex-wrap justify-center gap-4">
           {navigation.company.map((item) => (
             <a
               key={item.name}
               href={item.href}
               target="_blank"
               rel="noopener noreferrer"
-              className="text-sm leading-6 text-gray-600 hover:text-gray-900"
+              className="text-sm text-gray-600 hover:text-gray-900"
             >
               {item.name}
             </a>
           ))}
         </div>
 
-        <div className="flex items-center space-x-4">
+        {/* Second row (Socials) */}
+        <div className="flex justify-center gap-4">
           {navigation.social.map((item) => (
             <a
               key={item.name}
@@ -60,7 +61,7 @@ export default function Footer() {
               className="text-gray-400 hover:text-gray-500"
             >
               <span className="sr-only">{item.name}</span>
-              <item.icon aria-hidden="true" className="h-6 w-6" />
+              <item.icon aria-hidden="true" />
             </a>
           ))}
         </div>
